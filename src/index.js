27219526b234e5a8fc2455d7578876c090db4539
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-02-10 11:23:35 +0100

    redux install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Bucket } from "./components/Bucket/Bucket";
-import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
-
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Provider store={store}></Provider>
     <App />
   </BrowserRouter>
 );
